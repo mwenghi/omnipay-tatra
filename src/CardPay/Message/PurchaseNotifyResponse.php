085cea2e400aa59908b37ad649012c32e07ead71
mwenghi <mwenghi@gmail.com>
2018-07-17 17:21:43 +0200

    Create PurchaseNotifyResponse.php

---
+++ src/CardPay/Message/PurchaseNotifyResponse.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Omnipay\CardPay\Message;
+
+use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\NotificationInterface;
+use Omnipay\Common\Message\RedirectResponseInterface;
+use Omnipay\Core\Message\AbstractRequest;
+use Omnipay\Core\Sign\HmacVerify;
+
+class PurchaseNotifyResponse extends AbstractResponse implements NotificationInterface {
+
+    const RESPONSE_STATUS_OK        = 'OK';
+    const RESPONSE_STATUS_ERROR     = 'FAIL';
+
+    public function getTransactionStatus()
+    {
+        // TODO: Implement getTransactionStatus() method.
+    }
+
+    public function getMessage()
+    {
+        // TODO: Implement getMessage() method.
+    }
+
+    public function isSuccessful()
+    {
+        $verified = $this->verifyResponse();
+        return $verified;
+    }
+
+
+    private function verifyResponse()
+    {
+        $request = $this->getRequest();
+        $data = $this->data;
+
+        $stringToSign = [
+            $data["AMT"],
+            $data["CURR"],
+            $data["VS"],
+            $data["RES"],
+            $data["AC"],
+            $data["TID"],
+            $data["TIMESTAMP"],
+            $data["HMAC"]
+        ];
+
+        $sign = new HmacVerify();
+        return $sign->verify(implode("", $stringToSign), $data["ECDSA"], $data["ECDSA_KEY"])
+                && $data["RES"] == self::RESPONSE_STATUS_OK;
+    }
+
+
+}
